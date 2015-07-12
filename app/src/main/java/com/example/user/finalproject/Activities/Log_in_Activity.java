0b278a5cfedbb6e392990b89baa7795d4966e408
BeqaKhaburdzania <bkhab12@freeuni.edu.ge>
2015-07-12 13:37:36 +0400

    Initial Activities

---
+++ app/src/main/java/com/example/user/finalproject/Activities/Log_in_Activity.java
@@ -0,0 +1,7 @@
+package com.example.user.finalproject.Activities;
+
+
+import android.support.v7.app.ActionBarActivity;
+
+public class Log_in_Activity extends ActionBarActivity {
+}
---
+++ app/src/main/java/com/example/user/finalproject/Activities/Sign_Up_Activity.java
@@ -0,0 +1,7 @@
+package com.example.user.finalproject.Activities;
+
+
+import android.support.v7.app.ActionBarActivity;
+
+public class Sign_Up_Activity extends ActionBarActivity {
+}
